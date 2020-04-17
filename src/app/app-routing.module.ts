1a7a9472812e7abb6bedff86a5f1104344f934a4
Patricia Gomes <patriciagomesro33@gmail.com>
2020-04-16 23:33:38 -0400

    spa Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//Importando os componentes que vai ser usado nessa spa
+import { SobreComponent } from './sobre/sobre.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+//Inserindo as rotas dessa spa
+const routes: Routes = [
+	{ path: 'sobre', component: SobreComponent },
+	{ path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
